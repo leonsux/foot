9b68d910ecbfc55fd95096656fe9c47c684ad120
leonsuxgit config --global user.email leonsux@163.com <leonsux@163.com>
2018-01-11 21:14:46 +0800

    fix bug of router-active

--- src/components/tab-bar/AppTabBar.vue
+++ src/components/tab-bar/AppTabBar.vue
@@ -2,8 +2,11 @@
   <div class="tab-bar">
     <ul>
       <router-link :class="{'activeBar': activeBar == item.id ? true : false}"  v-for="item in bars" tag="li" :to="{name: item.name}">
-        <i :class="item.ico"></i>
-        <span @click="activeBar=item.id">浏览</span>
+        <div @click="activeBar=item.id">
+          <i :class="item.ico"></i>
+          <span>浏览</span>
+        </div>
+        
       </router-link>
     </ul>
   </div>
@@ -30,19 +33,8 @@
 
 <style lang="scss" >
 .tab-bar{
-  // &:before{
-  //   content: "";
-  //   display: block;
-  //   height: 1px;
-  //   background: red;
-
-  // }
-  // position: fixed;
-  // bottom: 0;
   width: 100%;
   height: 50px;
-  // background: #26A2FF;
-  // border-top: 2px #0ff solid;
   box-shadow: 0 -1px 2px #ccc;
   > ul{
   
@@ -50,21 +42,18 @@
     justify-content: space-around;
     align-items: center;
     height: 100%;
-    // color: #fff;
     > li{
-      // padding: 5px;
-      flex: 1;
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-      align-items: center;
-      height: 100%;
-      border: 0;
+      > div{
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        height: 100%;
+        border: 0;
+      }
     }
   }
-  // .router-link-exact-active{
-  //   color: #26A2FF;
-  // }
   .activeBar{
     color: red;
   }
